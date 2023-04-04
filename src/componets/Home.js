a46662d7d484cbef52f3a45935864e4fdc27e531
florencialarreategui <florencialarreategui@gmail.com>
2023-04-04 10:49:59 -0300

    inicio porfolio y creo componentes

---
+++ src/componets/Home.js
@@ -0,0 +1,14 @@
+import Menu from "./Menu"
+const Home = () =>{
+    return (
+        <div>
+            <div className="nombre-home">
+            <h1>Florencia</h1>
+            <h2>Larreategui</h2>
+            </div>
+            <Menu></Menu>
+        </div>
+
+    )
+}
+export default Home; 
---
+++ src/componets/Menu.js
@@ -0,0 +1,26 @@
+
+const Menu = () => {
+    return (
+        <div>
+            <ul>
+                <li>
+                    Sobre mi
+                </li>
+                <li>
+                    Proyectos web
+                </li>
+                <li>
+                    Diseños | ilustraciones
+                </li>
+                <li>
+                    Obras | fotografías
+                </li>
+                <li>
+                    Contacto
+                </li>
+            </ul>
+        </div>
+    )
+} 
+
+export default Menu; 
